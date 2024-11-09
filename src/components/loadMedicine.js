c983372d7a64c60b38dfb4420953367de229eade
danielquzhao <94013292+danielquzhao@users.noreply.github.com>
2024-11-09 17:38:06 -0500

    edit loadMedicine

---
+++ src/components/loadMedicine.js
@@ -0,0 +1,10 @@
+
+// src/components/loadMedicine.js
+import React from 'react';
+import './loadMedicine.css';
+
+const LoadMedicine = () => {
+    return <h1>hi</h1>
+}
+
+export default LoadMedicine;
