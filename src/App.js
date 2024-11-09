c790e7dde4c9b184c9b5f551492d56bc51e0d035
smathew32 <smathew32@myseneca.ca>
2024-11-09 17:45:48 -0500

    fixed site so it displays stuff

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Profile from './pages/profile';
+import Navbar from './components/Navbar'; // Assuming this is your Navbar component
+import Profile from './pages/profile';  // Page components
 import Home from './pages/Home';
 import About from './pages/about';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; // Include your styles if needed
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Edit <code>src/App.js</code> and save to reload.</p>
-        </header>
+      {/* Navbar is displayed on all pages */}
+      <Navbar />
+      {/* Routes for different pages */}
+      <div className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
